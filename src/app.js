02728eaf3bdbf6facd7047b1f0d20f2736aaf925
Mahmoud Alshahin <mahmoudsha198811@gmail.com>
2023-07-14 01:29:30 +0300

    Adding swagger ui to our app
    Adding swagger ui to our app

--- src/app.js
+++ src/app.js
@@ -3,16 +3,21 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
   /* eslint-enable global-require */
 }
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 const express = require('express');
-
+const bookRoutes = require('./routes/book');
+const options = require('./utils/swagger');
 const connectToMongo = require('./db/connection');
 
 const app = express();
 const port = process.env.NODE_LOCAL_PORT;
-
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/', bookRoutes);
+
+const specs = swaggerJsdoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
