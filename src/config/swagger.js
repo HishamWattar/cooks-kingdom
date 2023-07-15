53ec5f68a387df8ad103f72c015b6d5f33eddd0b
Mahmoud Alshahin <mahmoudsha198811@gmail.com>
2023-07-16 00:47:24 +0300

    setup swagger ui
    add swagger ui to project

---
+++ src/config/swagger.js
@@ -0,0 +1,28 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Cooks Kingdom',
+      version: '0.1.0',
+      description:
+        'This is Cooks kingdom  API application made with Express and documented with Swagger',
+      license: {
+        name: 'MIT',
+        url: 'https://spdx.org/licenses/MIT.html',
+      },
+      contact: {
+        name: 'Recoded Team 5',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000/',
+      },
+    ],
+  },
+  apis: ['./src/docs/**/*.yaml'],
+};
+
+module.exports = swaggerJSDoc(options);
