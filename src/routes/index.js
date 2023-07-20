651cb7ef7b165ec935717a8f4e7628616504dd76
Ahmad Alashtar <ahmadalashtar@gmail.com>
2023-07-20 20:59:25 +0300

    Use /api/dishes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const routes = express.Router();
 const authRoutes = require('./auth');
+const dishRoute = require('./dish');
 
 routes.use('/auth', authRoutes);
+routes.use('/dishes', dishRoute);
 
 module.exports = routes;
