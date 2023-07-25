a9c9aa830b0b90cf8e04978451950bda6adf5bac
Rawan <r.k096@hotmail.com>
2023-07-25 14:49:31 +0300

    cart

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,32 +1,34 @@
-/**
- * @swagger
- * tags:
- *   name: Cart
- *   description: API endpoints for managing the cart
- */
-
-/**
- * @swagger
- * /cart:
- *   post:
- *     summary: Create a new cart
- *     tags: [Cart]
- *     responses:
- *       201:
- *         description: Successfully created a new cart
- *       500:
- *         description: Failed to create cart
- */
 const express = require('express');
 const cartControllers = require('../controllers/cart');
+const { isAuthenticated, isCustomer } = require('../middlewares/auth');
 
 const routes = express.Router();
 
-routes.post('/', cartControllers.postCart);
-routes.delete('/', cartControllers.deleteCart);
-routes.post('/CartItem/:id', cartControllers.postCartItemByDishId);
-routes.put('CartItem/:id', cartControllers.putCartItemByDishId);
-routes.get('CartItem/:id', cartControllers.getCartItemByDishId);
-routes.delete('CartItem/:id', cartControllers.deleteCartItemByDishId);
+routes.post('/', isAuthenticated, isCustomer, cartControllers.postCart);
+routes.delete('/', isAuthenticated, isCustomer, cartControllers.deleteCart);
+routes.post(
+  '/CartItem/:id',
+  isAuthenticated,
+  isCustomer,
+  cartControllers.postCartItemByDishId
+);
+routes.put(
+  'CartItem/:id',
+  isAuthenticated,
+  isCustomer,
+  cartControllers.putCartItemByDishId
+);
+routes.get(
+  'CartItem/:id',
+  isAuthenticated,
+  isCustomer,
+  cartControllers.getCartItemByDishId
+);
+routes.delete(
+  'CartItem/:id',
+  isAuthenticated,
+  isCustomer,
+  cartControllers.deleteCartItemByDishId
+);
 
 module.exports = routes;
