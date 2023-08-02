6f460c795d97904bf7ca719036b29070548f7b04
Ahmad Alashtar <ahmadalashtar@gmail.com>
2023-08-02 19:59:52 +0300

    Create isOwner and update error handling

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,19 +1,23 @@
 const jwt = require('jsonwebtoken');
 const { User } = require('../models/user');
+const Dish = require('../models/dish');
+const Cart = require('../models/cart');
+const Order = require('../models/order');
+const CustomError = require('../utils/customError');
 
 // eslint-disable-next-line consistent-return
 const isAuthenticated = async (req, res, next) => {
   if (req.cookies) {
     const { token } = req.cookies;
     if (!token) {
-      return res.status(401).json({ message: 'Unauthenticated' });
+      return next(new CustomError('Unauthenticated', 401));
     }
     try {
       const decoded = jwt.verify(token, process.env.APP_SECRET);
       req.user = await User.findById({ _id: decoded.userId });
       return next();
     } catch (error) {
-      return res.status(401).json({ message: 'Invalid token' });
+      return next(new CustomError('Invalid token', 401));
     }
   }
 };
@@ -25,26 +29,63 @@ const isChef = (req, res, next) => {
   ) {
     return next();
   }
-  return res.status(403).json({ message: 'This action is unauthorized' });
+  return next(new CustomError('This action is unauthorized', 403));
 };
 
 const isCustomer = (req, res, next) => {
   if (req.user.role === 'customer' || req.user.role === 'admin') {
     return next();
   }
-  return res.status(403).json({ message: 'This action is unauthorized' });
+  return next(new CustomError('This action is unauthorized', 403));
 };
 
 const isAdmin = (req, res, next) => {
   if (req.user.role === 'admin') {
     return next();
   }
-  return res.status(403).json({ message: 'This action is unauthorized' });
+  return next(new CustomError('This action is unauthorized', 403));
 };
+
+function isOwner(path) {
+  return async (req, res, next) => {
+    const userId = req.user._id.toString();
+    const { id } = req.params;
+    try {
+      let owner = false;
+      if (path === 'dishes') {
+        const dish = await Dish.findById(id);
+        owner = dish && dish.chefId.toString() === userId;
+      } else if (path === 'cart') {
+        const cart = await Cart.findById(id);
+        owner = cart && cart.customerId.toString() === userId;
+      } else if (path === 'orders') {
+        const order = await Order.findById(id);
+        owner = order && order.customerId.toString() === userId;
+      } else {
+        // If the provided 'path' is invalid, you can handle it here
+        return next(new CustomError('Invalid path', 400));
+      }
+      if (!owner) {
+        return next(
+          new CustomError(
+            'Unauthorized: You are not the owner of this resource',
+            403
+          )
+        );
+      }
+    } catch (err) {
+      // Handle any potential errors
+      return next();
+    }
+    // If the user is the owner, continue to the next middleware or route handler
+    return next();
+  };
+}
 
 module.exports = {
   isAuthenticated,
   isAdmin,
   isCustomer,
   isChef,
+  isOwner,
 };
