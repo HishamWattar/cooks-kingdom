3be6590d7fc4a517094d7e22755e6c179e5d7cdf
Rawan <r.k096@hotmail.com>
2023-08-06 20:31:22 +0300

    cart ui only

--- src/controllers/front/home.js
+++ src/controllers/front/home.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const Cart = require('../../models/cart');
 const dishModel = require('../../models/dish');
 const { User } = require('../../models/user');
 const CustomError = require('../../utils/customError');
@@ -16,6 +17,22 @@ const getAllDishes = async (req, res, next) => {
   }
 };
 
+const getCart = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    const decoded = jwt.verify(token, process.env.APP_SECRET);
+    const user = await User.findById({ _id: decoded.userId });
+
+    const [cart] = await Cart.find({ customerId: user.id })
+      .populate('cartItems.dishId')
+      .exec();
+    const { cartItems } = cart.cartItems;
+    return res.render('cart', { cartItems, user });
+  } catch (err) {
+    return next(new CustomError(err.message, 500));
+  }
+};
+
 const login = async (req, res) => {
   res.render('login');
 };
@@ -28,4 +45,5 @@ module.exports = {
   getAllDishes,
   login,
   signup,
+  getCart,
 };
--- src/routes/front/index.js
+++ src/routes/front/index.js
@@ -4,6 +4,7 @@ const homeController = require('../../controllers/front/home');
 const routes = express.Router();
 
 routes.get('/', homeController.getAllDishes);
+routes.get('/cart', homeController.getCart);
 routes.get('/login', homeController.login);
 routes.get('/signup', homeController.signup);
 
