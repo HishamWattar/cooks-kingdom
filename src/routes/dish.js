e109f284e301f8572247f640fe11a4ce6cc79de5
Hisham Al Wattar <h.wattar@netro.xyz>
2023-08-07 20:27:23 +0300

    Update dish endpoints

--- src/routes/dish.js
+++ src/routes/dish.js
@@ -1,34 +1,29 @@
-/* eslint-disable prettier/prettier */
 const express = require('express');
 
 const router = express.Router();
 
 const dishController = require('../controllers/dish');
-const { isAuthenticated, isChef, isOwner } = require('../middlewares/auth');
+const { isAuthenticated, isChef } = require('../middlewares/auth');
+const upload = require('../middlewares/multer');
 
 router.get('/', dishController.getAllDishes);
+
 router.get('/filter', dishController.filterDishes);
+
 router.get('/:id', dishController.getDishById);
 
 router.post('/', isAuthenticated, isChef, dishController.addDish);
-router.put(
-  '/:id',
-  isAuthenticated,
-  isChef,
-  isOwner('dishes'),
-  dishController.updateDish
-);
-router.delete(
-  '/:id',
-  isAuthenticated,
-  isOwner('dishes'),
-  isChef,
-  dishController.deleteDish
-);
-router.post('/image', isAuthenticated, isChef, dishController.uploadDishImage);
 
-// router.post('/',  dishController.addDish);
-// router.put('/:id',  dishController.updateDish);
-// router.delete('/:id',  dishController.deleteDish);
+router.put('/:id', isAuthenticated, isChef, dishController.updateDish);
+
+router.delete('/:id', isAuthenticated, isChef, dishController.deleteDish);
+
+router.put(
+  '/image/:id',
+  isAuthenticated,
+  isChef,
+  upload.single('image'),
+  dishController.uploadDishImage
+);
 
 module.exports = router;
