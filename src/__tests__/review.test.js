2daa533c59fbbda7b8b4054442cfcca2fd253541
Hisham Al Wattar <h.wattar@netro.xyz>
2023-08-07 17:25:57 +0300

    Fix reviews tests

--- src/__tests__/review.test.js
+++ src/__tests__/review.test.js
@@ -1,14 +1,11 @@
 const supertest = require('supertest');
 const app = require('../app');
 const db = require('../db/connection');
-const dishModel = require('../models/dish');
-const { User } = require('../models/user');
-
+const Dish = require('../models/dish');
+// const { User } = require('../models/user');
 const req = supertest(app);
 let customerToken;
-let customerId;
 let fishId;
-let allDishes;
 
 const customerUser = {
   firstName: 'customerTest',
@@ -28,56 +25,25 @@ const fish = {
     'Vegetable oil',
     'Yogurt marinade',
   ],
-  reviews: [
-    {
-      rate: 5,
-      description: 'Flavours were spot on, really tasty dish.',
-    },
-    {
-      rate: 3,
-      description: 'A bit low on spice for my liking.',
-    },
-  ],
-};
-const chicken = {
-  name: 'Chicken Tikka Masala',
-  price: 10,
-  ingredients: [
-    'Chicken thighs',
-    'Yogurt marinade',
-    'Tomato sauce',
-    'Spices',
-    'Potatoes',
-  ],
-  reviews: [
-    {
-      rate: 5,
-      description: 'Flavours were spot on, really tasty dish.',
-    },
-    {
-      rate: 3,
-      description: 'A bit low on spice for my liking.',
-    },
-  ],
 };
 
 beforeAll(async () => {
   await db.connectToMongo();
   const res = await req.post('/api/auth/signup').send(customerUser);
   [customerToken] = res.headers['set-cookie'][0].split(';');
-  customerId = res.body.data._id;
-  allDishes = await dishModel.insertMany([fish, chicken]);
-  fishId = allDishes[0]._id;
+  const dish = await Dish.create(fish);
+  fishId = dish._id;
 });
 
 afterAll(async () => {
-  await dishModel.deleteMany({});
-  await User.deleteMany({});
+  // await Dish.deleteMany({});
+  // await User.deleteMany({});
+  await db.clearDatabase();
   await db.closeDatabase();
 });
 
 describe('POST /api/reviews/:id', () => {
-  test('should return 404 if no rating provided', async () => {
+  it('should return 422 if no rate provided', async () => {
     const res = await req
       .post(`/api/reviews/${fishId.toString()}`)
       .set('Cookie', customerToken)
@@ -86,27 +52,39 @@ describe('POST /api/reviews/:id', () => {
       });
     expect(res.statusCode).toBe(422);
   });
-  test('should return 201 if review created', async () => {
+
+  it('should return 201 if review created', async () => {
     const res = await req
       .post(`/api/reviews/${fishId.toString()}`)
       .set('Cookie', customerToken)
       .send({
-        customerId,
         rate: 5,
         comment: 'Great dish',
       });
+
     expect(res.statusCode).toBe(201);
+  });
+
+  it('should return 400 if customer already reviewed the dish', async () => {
+    const res = await req
+      .post(`/api/reviews/${fishId.toString()}`)
+      .set('Cookie', customerToken)
+      .send({
+        rate: 5,
+        comment: 'Great dish',
+      });
+    expect(res.statusCode).toBe(400);
   });
 });
 describe('PUT /api/reviews/:id', () => {
-  test('should return 401 if no token provided', async () => {
+  it('should return 401 if no token provided', async () => {
     const res = await req.put(`/api/reviews/${fishId.toString()}`).send({
       rate: 5,
       comment: 'Great dish',
     });
     expect(res.statusCode).toBe(401);
   });
-  test('should return 200 if review updated', async () => {
+  it('should return 200 if review updated', async () => {
     const res = await req
       .put(`/api/reviews/${fishId.toString()}`)
       .set('Cookie', customerToken)
@@ -114,15 +92,15 @@ describe('PUT /api/reviews/:id', () => {
         rating: 5,
         comment: 'Great dish',
       });
-    expect(res.statusCode).toBe(201);
+    expect(res.statusCode).toBe(200);
   });
 });
 describe('DELETE /api/reviews/:id', () => {
-  test('should return 401 if no token provided', async () => {
+  it('should return 401 if no token provided', async () => {
     const res = await req.delete(`/api/reviews/${fishId}`);
     expect(res.statusCode).toBe(401);
   });
-  test('should return 204 if review deleted', async () => {
+  it('should return 204 if review deleted', async () => {
     const res = await req
       .delete(`/api/reviews/${fishId}`)
       .set('Cookie', customerToken);
