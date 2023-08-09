2c0161bb03ed7f6d53ce131bf4d402398cd9ef8e
Ahmad Alashtar <ahmadalashtar@gmail.com>
2023-08-09 19:27:35 +0200

    Update config/swagger.js

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -6,19 +6,28 @@ const options = {
     info: {
       title: 'Cooks Kingdom',
       version: '0.1.0',
-      description:
-        'This is Cooks kingdom  API application made with Express and documented with Swagger',
+      description: `Cooks Kingdom is a platform that connects busy individuals, like students, with local cooks offering
+        homemade food, providing convenient and delicious meal options. The website features an
+        easy-to-use interface, allowing customers to browse dishes, place orders, and support local
+        cookers, while chefs can manage their offerings through an admin dashboard.
+        Check out our [GitHub repository](https://github.com/HishamWattar/cooks-kingdom), our [system architecture](https://github.com/HishamWattar/cooks-kingdom), and our [database design](https://github.com/HishamWattar/cooks-kingdom)`,
+      contact: {
+        email: 'cooks.supp@gmail.com',
+      },
       license: {
         name: 'MIT',
         url: 'https://spdx.org/licenses/MIT.html',
       },
-      contact: {
-        name: 'Recoded Team 5',
-      },
+    },
+    externalDocs: {
+      description: 'Find out more',
+      url: 'http://swagger.io',
     },
     servers: [
       {
-        url: 'http://localhost:3000/',
+        url: process.env.PRODUCTION
+          ? 'https://cookskingdom.onrender.com'
+          : 'http://localhost:3000/',
       },
     ],
   },
